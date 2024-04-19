80877cd68a435ab75cd3ce4e888d589d11105618
nitishxyz <nitishxyz@proton.me>
2024-04-19 11:35:09 +0530

    added format in save

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,82 +4,80 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
-vim.g.mapleader = " " 
+vim.g.mapleader = " "
 
 -- Navigate vim panes better
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
 
 vim.cmd("set ignorecase")
 vim.cmd("set smartcase")
 -- Set clipboard to system clipboard
 vim.cmd("set clipboard=unnamedplus")
 
-
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
 
 -- Copy to clipboard
-vim.keymap.set('n', '<A-y>', '+y')
-vim.keymap.set('n', '<A-Y>', '+Y')
-vim.keymap.set('n', '<A-yy>', '+yy')
+vim.keymap.set("n", "<A-y>", "+y")
+vim.keymap.set("n", "<A-Y>", "+Y")
+vim.keymap.set("n", "<A-yy>", "+yy")
 
 -- Paste from clipboard
-vim.keymap.set('n', '<A-p>', '+p')
-vim.keymap.set('n', '<A-P>', '+P')
+vim.keymap.set("n", "<A-p>", "+p")
+vim.keymap.set("n", "<A-P>", "+P")
 
 -- Insert mode mappings
-vim.keymap.set('i', '<C-h>', '<Left>')
-vim.keymap.set('i', '<C-j>', '<Down>')
-vim.keymap.set('i', '<C-k>', '<Up>')
-vim.keymap.set('i', '<C-l>', '<Right>')
+vim.keymap.set("i", "<C-h>", "<Left>")
+vim.keymap.set("i", "<C-j>", "<Down>")
+vim.keymap.set("i", "<C-k>", "<Up>")
+vim.keymap.set("i", "<C-l>", "<Right>")
 
 -- Move in insert mode
-vim.keymap.set('i', '<A-a>', '<Home>')
-vim.keymap.set('i', '<A-e>', '<End>')
-vim.keymap.set('i', '<A-h>', '<Left>')
-vim.keymap.set('i', '<A-j>', '<Down>')
-vim.keymap.set('i', '<A-k>', '<Up>')
-vim.keymap.set('i', '<A-l>', '<Right>')
-vim.keymap.set('i', '<A-d>', '<Del>')
-vim.keymap.set('i', '<A-b>', '<C-Left>')
-vim.keymap.set('i', '<A-f>', '<C-Right>')
-vim.keymap.set('i', '<A-w>', '<C-Right>')
-
+vim.keymap.set("i", "<A-a>", "<Home>")
+vim.keymap.set("i", "<A-e>", "<End>")
+vim.keymap.set("i", "<A-h>", "<Left>")
+vim.keymap.set("i", "<A-j>", "<Down>")
+vim.keymap.set("i", "<A-k>", "<Up>")
+vim.keymap.set("i", "<A-l>", "<Right>")
+vim.keymap.set("i", "<A-d>", "<Del>")
+vim.keymap.set("i", "<A-b>", "<C-Left>")
+vim.keymap.set("i", "<A-f>", "<C-Right>")
+vim.keymap.set("i", "<A-w>", "<C-Right>")
 
 -- Move to start/end of line
 -- vim.keymap.set('n', 'H', '^')
 -- vim.keymap.set('n', 'L', '$')
 
--- 
+--
 vim.wo.number = true
 
 local opts = { noremap = true, silent = true }
 
 -- Move to previous/next
-map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
-map('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
+map("n", "<A-,>", "<Cmd>BufferPrevious<CR>", opts)
+map("n", "<A-.>", "<Cmd>BufferNext<CR>", opts)
 -- Re-order to previous/next
-map('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts)
-map('n', '<A->>', '<Cmd>BufferMoveNext<CR>', opts)
+map("n", "<A-<>", "<Cmd>BufferMovePrevious<CR>", opts)
+map("n", "<A->>", "<Cmd>BufferMoveNext<CR>", opts)
 -- Goto buffer in position...
-map('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
-map('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)
-map('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', opts)
-map('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', opts)
-map('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', opts)
-map('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', opts)
-map('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', opts)
-map('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', opts)
-map('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', opts)
-map('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
+map("n", "<A-1>", "<Cmd>BufferGoto 1<CR>", opts)
+map("n", "<A-2>", "<Cmd>BufferGoto 2<CR>", opts)
+map("n", "<A-3>", "<Cmd>BufferGoto 3<CR>", opts)
+map("n", "<A-4>", "<Cmd>BufferGoto 4<CR>", opts)
+map("n", "<A-5>", "<Cmd>BufferGoto 5<CR>", opts)
+map("n", "<A-6>", "<Cmd>BufferGoto 6<CR>", opts)
+map("n", "<A-7>", "<Cmd>BufferGoto 7<CR>", opts)
+map("n", "<A-8>", "<Cmd>BufferGoto 8<CR>", opts)
+map("n", "<A-9>", "<Cmd>BufferGoto 9<CR>", opts)
+map("n", "<A-0>", "<Cmd>BufferLast<CR>", opts)
 -- Pin/unpin buffer
-map('n', '<A-p>', '<Cmd>BufferPin<CR>', opts)
+map("n", "<A-p>", "<Cmd>BufferPin<CR>", opts)
 -- Close buffer
-map('n', '<A-c>', '<Cmd>BufferClose<CR>', opts)
+map("n", "<A-c>", "<Cmd>BufferClose<CR>", opts)
 -- Wipeout buffer
 --                 :BufferWipeout
 -- Close commands
@@ -89,12 +87,12 @@ map('n', '<A-c>', '<Cmd>BufferClose<CR>', opts)
 --                 :BufferCloseBuffersLeft
 --                 :BufferCloseBuffersRight
 -- Magic buffer-picking mode
-map('n', '<C-p>', '<Cmd>BufferPick<CR>', opts)
+--map("n", "<C-p>", "<Cmd>BufferPick<CR>", opts)
 -- Sort automatically by...
-map('n', '<Space>bb', '<Cmd>BufferOrderByBufferNumber<CR>', opts)
-map('n', '<Space>bd', '<Cmd>BufferOrderByDirectory<CR>', opts)
-map('n', '<Space>bl', '<Cmd>BufferOrderByLanguage<CR>', opts)
-map('n', '<Space>bw', '<Cmd>BufferOrderByWindowNumber<CR>', opts)
+map("n", "<Space>bb", "<Cmd>BufferOrderByBufferNumber<CR>", opts)
+map("n", "<Space>bd", "<Cmd>BufferOrderByDirectory<CR>", opts)
+map("n", "<Space>bl", "<Cmd>BufferOrderByLanguage<CR>", opts)
+map("n", "<Space>bw", "<Cmd>BufferOrderByWindowNumber<CR>", opts)
 
 -- Other:
 -- :BarbarEnable - enables barbar (enabled by default)
@@ -102,10 +100,16 @@ map('n', '<Space>bw', '<Cmd>BufferOrderByWindowNumber<CR>', opts)
 --
 --
 
-
 -- mapping to reload LSP
-vim.keymap.set('n', '<leader>l', ':LspRestart<CR>')
-
+vim.keymap.set("n", "<leader>l", ":LspRestart<CR>")
 
 -- mapping for command palette :!
-map('n', '<leader>k', ':!', { noremap = true, silent = false})
+map("n", "<leader>k", ":!", { noremap = true, silent = false })
+
+-- Define an autocmd for BufWritePost event to trigger formatting
+vim.cmd([[
+    augroup auto_format_on_save
+        autocmd!
+        autocmd BufWritePost * lua vim.lsp.buf.format()
+    augroup END
+]])

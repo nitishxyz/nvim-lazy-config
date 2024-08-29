ff818e9e4b66f752ca77d1c6ebafa9826420009b
nitishxyz <nitishxyz@proton.me>
2024-08-30 04:01:05 +0530

    reverted back to main variant of rose pine

--- lua/plugins/rose-pine.lua
+++ lua/plugins/rose-pine.lua
@@ -3,7 +3,7 @@ return {
 	name = "rose-pine",
 	config = function()
 		local rose_pine = require("rose-pine")
-		rose_pine.setup({ variant = "moon", dim_inactive_windows = true })
+		rose_pine.setup({ dim_inactive_windows = true })
 		vim.cmd.colorscheme("rose-pine")
 	end,
 }

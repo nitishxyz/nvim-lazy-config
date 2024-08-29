dffc84b266ce95922a250dff62dd853cc835bba9
nitishxyz <nitishxyz@proton.me>
2024-08-30 03:56:18 +0530

    update theme to moon

--- lua/plugins/rose-pine.lua
+++ lua/plugins/rose-pine.lua
@@ -2,8 +2,8 @@ return {
 	"rose-pine/neovim",
 	name = "rose-pine",
 	config = function()
-    local rose_pine = require("rose-pine")
-    rose_pine.setup({ dim_inactive_windows = true })
-    vim.cmd.colorscheme("rose-pine")
-  end,
+		local rose_pine = require("rose-pine")
+		rose_pine.setup({ variant = "moon", dim_inactive_windows = true })
+		vim.cmd.colorscheme("rose-pine")
+	end,
 }
